4526186f1f5a274ba5ed0de4b856d1550868f38b
Eero Rantala <eero.rantala@nitor.com>
2024-03-26 15:40:56 +0200

    Osa 2c puhelinluettelo done

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Search from './components/Search'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
+const notesService = 'http://localhost:3001/persons'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    axios.get(notesService).then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const searchedPersons = persons.filter(person =>
     person.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -47,7 +51,7 @@ const App = () => {
         setNewNumber={(event) => setNewNumber(event.target.value)}
         handleClick={handleClick}
       />
-      
+
       <h2>Numbers</h2>
       <Persons persons={searchedPersons} />
     </div>
