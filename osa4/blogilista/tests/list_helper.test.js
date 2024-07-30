5bb7f27b4935ed0fb1a30e31e02b5337b5210157
Eero Rantala <eero.rantala@nitor.com>
2024-07-30 14:21:32 +0300

    Tehtävät 4.5 ja 4.6

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -6,7 +6,7 @@ const blogs = require('./blogs_for_test')
 test('dummy returns one', () => {
   const blogs = []
 
-  const result = listHelper.dummy(blogs)
+  const result = listHelper.dummy(blogs.ALL)
   assert.strictEqual(result, 1)
 })
 
@@ -17,12 +17,60 @@ describe('total likes', () => {
   })
 
   test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(blogs.slice(0, 1))
+    const result = listHelper.totalLikes([blogs.reactPatterns])
     assert.strictEqual(result, 7)
   })
 
   test('of a bigger list is calculated rigth', () => {
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes(blogs.ALL)
     assert.strictEqual(result, 36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
+  })
+
+  test('when list has only one blog equals that', () => {
+    const result = listHelper.favoriteBlog([blogs.tddHarmsArchitecture])
+    assert.deepStrictEqual(result, {
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      likes: 0
+    })
+  })
+
+  test('of a bigger list is the one with most likes', () => {
+    const result = listHelper.favoriteBlog(blogs.ALL)
+    assert.deepStrictEqual(result, {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    })
+  })
+})
+
+describe('most blogs', () => {
+  test('of empty list is null', () => {
+    const result = listHelper.mostBlogs([])
+    assert.strictEqual(result, null)
+  })
+
+  test('when list has only one blog equals that', () => {
+    const result = listHelper.mostBlogs([blogs.tddHarmsArchitecture])
+    assert.deepStrictEqual(result, {
+      author: 'Robert C. Martin',
+      blogs: 1
+    })
+  })
+
+  test('of a bigger list is the author with most blogs', () => {
+    const result = listHelper.mostBlogs(blogs.ALL)
+    assert.deepStrictEqual(result, {
+      author: 'Robert C. Martin',
+      blogs: 3
+    })
   })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -9,3 +9,34 @@ exports.totalLikes = blogs => {
 
   return blogs.reduce((sumLikes, blog) => sumLikes + blog.likes, 0)
 }
+
+exports.favoriteBlog = blogs => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const favoriteBlog = blogs.reduce((favorite, blog) => favorite.likes > blog.likes ? favorite : blog)
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  }
+}
+
+exports.mostBlogs = blogs => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const authors = blogs.reduce((authors, blog) => {
+    authors[blog.author] = authors[blog.author] ? authors[blog.author] + 1 : 1
+    return authors
+  }, {})
+
+  const initialValue = { author: '', blogs: 0 }
+  const authorWithMostBlogs = Object.entries(authors).reduce(
+    (authorWithMostBlogs, [author, blogs]) => {
+      return authorWithMostBlogs.blogs > blogs ? authorWithMostBlogs : { author, blogs: blogs }
+    }, initialValue)
+  return authorWithMostBlogs
+}
