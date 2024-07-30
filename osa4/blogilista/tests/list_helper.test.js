3563759638f27cac5f013b160c8134932496436c
Eero Rantala <eero.rantala@nitor.com>
2024-07-30 12:34:29 +0300

    Tehtävä 4.4

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -1,10 +1,28 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const blogs = require('./blogs_for_test')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogs.slice(0, 1))
+    assert.strictEqual(result, 7)
+  })
+
+  test('of a bigger list is calculated rigth', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 36)
+  })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,11 @@
 exports.dummy = blogs => {
   return 1
 }
+
+exports.totalLikes = blogs => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  return blogs.reduce((sumLikes, blog) => sumLikes + blog.likes, 0)
+}
