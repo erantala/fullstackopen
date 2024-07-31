fa34269747dbb6f84a5467e694c2bb18e2ff8b23
Eero Rantala <eero.rantala@nitor.com>
2024-07-31 16:59:39 +0300

    Tehtävä 4.10 blogilistan testit step3

--- osa4/blogilista/tests/blogs.test.js
+++ osa4/blogilista/tests/blogs.test.js
@@ -35,6 +35,22 @@ describe('blogilistan testit', () => {
     assert(firstBlog.id.match(/^[0-9a-f]+$/))
   })
 
+  test('kantaan voi lisätä blogeja', async () => {
+    const cantParseHtml = {
+      title: 'You can\'t parse [X]HTML with regex',
+      author: 'bobince',
+      url: 'https://stackoverflow.com/a/1732454'
+    }
+
+    const newBlog = new Blog(cantParseHtml)
+    await newBlog.save()
+
+    const allBlogs = await Blog.find({})
+    assert.strictEqual(allBlogs.length, blogs.ALL.length + 1)
+    const blogTitles = allBlogs.map(blog => blog.title)
+    assert(blogTitles.includes('You can\'t parse [X]HTML with regex'))
+  })
+
   after(async () => {
     await mongoose.connection.close()
     console.log('mongoose connection closed')
