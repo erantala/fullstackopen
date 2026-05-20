3bfc2c37d461b3405125f94962b0e2092d73d725
Eero Rantala <eero.rantala@nitor.com>
2026-05-20 14:45:36 +0300

    5.14: blogilistan testit, step2

--- osa5/blogilista-frontend/src/components/Blog.test.jsx
+++ osa5/blogilista-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Blog title is rendered', () => {
@@ -11,4 +12,29 @@ test('Blog title is rendered', () => {
 
   const element = screen.getByText('Make component testing great again')
   expect(element).toBeDefined()
+})
+
+test('Url, likes and user are shown when view button is clicked', async () => {
+  const blogUser = {
+    name: 'James David Vance',
+    username: 'jdvance'
+  }
+
+  const blog = {
+    title: 'Make component testing great again',
+    author: 'Donakd T.',
+    user: blogUser,
+    url: 'http://gov.us/donald/blog',
+    likes: 1
+  }
+
+  render(<Blog blog={blog} loggedUser={blogUser} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  screen.getByText('http://gov.us/donald/blog', { exact: false })
+  screen.getByText('likes 1', { exact: false })
+  screen.getByText('James David Vance', { exact: false })
 })
