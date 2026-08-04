6ac50c3fa2c192c4fcd284b493dfa9fb97be1268
Eero Rantala <eero.rantala@nitor.com>
2026-08-04 10:47:49 +0300

    5.18: blogilistan end to end -testit, step2
    Testit kirjautumiselle. Testaa sekä onnistunut että epäonnistunut
    kirjautuminen.

--- osa5/e2e-test/tests/blogilista_app.spec.js
+++ osa5/e2e-test/tests/blogilista_app.spec.js
@@ -1,5 +1,11 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
+const loginWith = async (page, username, password) => {
+  await page.getByLabel('username').fill(username)
+  await page.getByLabel('password').fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     await request.post('http://localhost:3003/api/testing/reset')
@@ -19,5 +25,24 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password:')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'playwright', 'playwleft-secret')
+      await page.getByText('login successful').waitFor()
+
+      await expect(page.getByText('P. Lay Wright logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'playwrong', 'asdasdas')
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toBeVisible()
+      await expect(errorDiv).toContainText('wrong credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
   })
 })
